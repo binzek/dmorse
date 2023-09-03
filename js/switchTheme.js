4a6a9391d3b61eaec84e2e7c1f3990444b6e25ed
Abdul Wajid <abdulwajidnv@gmail.com>
2023-09-03 19:40:27 +0530

    Enable local storage for theme

--- js/switchTheme.js
+++ js/switchTheme.js
@@ -3,12 +3,22 @@ const themeSwitch = document.getElementById("theme-switch");
 const themeSwitchLight = document.getElementById("theme-switch--light");
 const themeSwitchDark = document.getElementById("theme-switch--dark");
 
+window.addEventListener("load", () => {
+  if (!localStorage.theme) {
+    localStorage.theme = "light";
+  }
+  root.setAttribute("data-theme", localStorage.theme);
+  themeSwitch.setAttribute("data-active", localStorage.theme);
+});
+
 themeSwitchLight.addEventListener("click", () => {
   root.setAttribute("data-theme", "light");
   themeSwitch.setAttribute("data-active", "light");
+  localStorage.theme = "light";
 });
 
 themeSwitchDark.addEventListener("click", () => {
   root.setAttribute("data-theme", "dark");
   themeSwitch.setAttribute("data-active", "dark");
+  localStorage.theme = "dark";
 });
