ae80e1efb8228668b6e8c94c3aceecbb688fff3c
Abdul Wajid <abdulwajidnv@gmail.com>
2023-09-08 08:29:25 +0530

    Implement feature for encoding text

---
+++ js/encodeText.js
@@ -0,0 +1,74 @@
+const morseCode = {
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+  0: "-----",
+  1: ".----",
+  2: "..---",
+  3: "...--",
+  4: "....-",
+  5: ".....",
+  6: "-....",
+  7: "--...",
+  8: "---..",
+  9: "----.",
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "'": ".----.",
+  "!": "-.-.--",
+  "/": "-..-.",
+  "(": "-.--.",
+  ")": "-.--.-",
+  "&": ".-...",
+  ":": "---...",
+  ";": "-.-.-.",
+  "=": "-...-",
+  "+": ".-.-.",
+  "-": "-....-",
+  _: "..--.-",
+  '"': ".-..-.",
+  "@": ".--.-.",
+  " ": "/",
+};
+
+const textInput = document.getElementById("input--text");
+const morseOutput = document.getElementById("output--morse");
+const encodeButton = document.getElementById("button--encode");
+const copyCodeButton = document.getElementById("button--copy-code");
+
+encodeButton.addEventListener("click", () => {
+  morseOutput.value = textInput.value
+    .toUpperCase()
+    .split("")
+    .map((char) => morseCode[char] || char)
+    .join(" ");
+});
+
+copyCodeButton.addEventListener("click", () => {
+  morseOutput.select();
+  document.execCommand("copy");
+});
