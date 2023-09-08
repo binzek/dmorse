d831c11a8b041a93ea85f4d5b69038bcc54df44f
Abdul Wajid <abdulwajidnv@gmail.com>
2023-09-08 17:09:27 +0530

    Register service worker

---
+++ js/app.js
@@ -0,0 +1,10 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((res) => console.log("Service worker registered"))
+      .catch((err) =>
+        console.log("Service worker could not be registered", err)
+      );
+  });
+}
