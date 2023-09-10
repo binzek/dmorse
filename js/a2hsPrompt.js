acbe9f6263a62605ce87322d6b5ecda71d7a9af4
Abdul Wajid <abdulwajidnv@gmail.com>
2023-09-10 08:30:08 +0530

    Fix UI issues when a2hs overlay shows

--- js/a2hsPrompt.js
+++ js/a2hsPrompt.js
@@ -1,5 +1,6 @@
 let deferredPrompt;
 
+const body = document.querySelector("body");
 const a2hsScreen = document.getElementById("a2hs-screen");
 const a2hsInstallButton = document.getElementById("a2hs-button--install");
 const a2hsCancelButton = document.getElementById("a2hs-button--cancel");
@@ -8,14 +9,17 @@ window.addEventListener("beforeinstallprompt", (event) => {
   event.preventDefault();
   deferredPrompt = event;
   a2hsScreen.style.display = "flex";
+  body.classList.add("noscroll");
 });
 
 a2hsCancelButton.addEventListener("click", () => {
   a2hsScreen.style.display = "none";
+  body.classList.remove("noscroll");
 });
 
 a2hsInstallButton.addEventListener("click", () => {
   a2hsScreen.style.display = "none";
+  body.classList.remove("noscroll");
   deferredPrompt.prompt();
   deferredPrompt = null;
 });
